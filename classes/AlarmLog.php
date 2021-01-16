b65f3055447dff871df3a6ea8b979e460ae7af68
billchetch <45006134+billchetch@users.noreply.github.com>
2021-01-17 01:42:21 +0800

    Update AlarmLog.php

--- classes/AlarmLog.php
+++ classes/AlarmLog.php
@@ -5,8 +5,10 @@ class AlarmLog extends \chetch\db\DBObject{
 		$t = \chetch\Config::get('ALARM_LOG_TABLE', 'alarm_log');
 		self::setConfig('TABLE_NAME', $t);
 		
+		$at = \chetch\Config::get('ALARMS_TABLE', 'alarms');
+		
 		$tzo = self::tzoffset();
-		$sql = "SELECT *, CONCAT(created,' ', '$tzo') AS created FROM $t";
+		$sql = "SELECT l.id, l.alarm_state, l.alarm_message, l.comments, CONCAT(l.created,' ', '$tzo') AS created, a.alarm_id, a.alarm_name FROM $t AS l INNER JOIN $at AS a ON l.alarm_id=a.id";
 		self::setConfig('SELECT_SQL', $sql);
 		
 		self::setConfig('SELECT_DEFAULT_FILTER', "created<':to' AND created>=':from'");
